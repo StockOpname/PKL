bf8beaf7d8acbf41d34e3352acce78c45867d781
Abyan <harlem.minion09@gmail.com>
2020-01-07 10:48:39 +0700

    first commit

---
+++ src/stock_opname/Stock_Opname.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stock_opname;
+
+/**
+ *
+ * @author Abyan
+ */
+public class Stock_Opname {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
